acf523bed2bdd2cc1061c9c4cf047ba38a8223a5
Simge <sbusee2002@gmail.com>
2023-05-10 10:26:20 +0300

    circle added and rotated

---
+++ Assets/Scripts/HalkaKontrol.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalkaKontrol : MonoBehaviour
+{
+    public float donmeHizi;
+    public bool soladon = true;
+
+    private void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (soladon)//sol
+        {
+            transform.Rotate(0f, 0f, donmeHizi * Time.deltaTime);
+        }
+        else//sağ
+        {
+            transform.Rotate(0f, 0f, -donmeHizi * Time.deltaTime);
+        }
+    }
+}
